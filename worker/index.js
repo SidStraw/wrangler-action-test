b86a97ffbdc7d5a785a52843779f886dff400d98
SidStraw <j80304xz@gmail.com>
2022-09-02 19:05:26 +0800

    style: js format

--- worker/index.js
+++ worker/index.js
@@ -1,7 +1,7 @@
-addEventListener("fetch", event => {
-    event.respondWith(handleRequest(event.request))
-  })
-  
-  async function handleRequest(request) {
-    return new Response("Hello world")
-  }
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  return new Response('Hello Sid')
+}
